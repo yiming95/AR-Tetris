26f0c9c55df7ce2c58d6dc28ab290a075cca1296
Owen <yuhuaqing411739@gmail.com>
2019-09-20 13:50:32 +1000

    comments and format

--- Assets/Tetris Template/Scripts/Utils/Constants.cs
+++ Assets/Tetris Template/Scripts/Utils/Constants.cs
@@ -6,9 +6,9 @@ public static class Constants
 {
     public static string GAME_NAME = "AR Tetris";
     public static string ASSETSTORE_URL = "https://github.com/Kartoro/COMP90018_MobileApp";
-    public static string ONLINE_DOC_URL = "https://goo.gl/pekuTQ";
-    public static string PATREON_URL = "https://www.patreon.com/skardgames";
-    public static string FACEBOOK_URL = "https://www.facebook.com/Skard-Games-1268859439806011/";
-    public static string TWITTER_URL = "https://twitter.com/skardgame";
-    public static string CONTACT_URL = "https://baturalpgurdin.github.io/#/contact";
+    public static string ONLINE_DOC_URL = "https://github.com/Kartoro/COMP90018_MobileApp";
+    public static string PATREON_URL = "https://github.com/Kartoro/COMP90018_MobileApp";
+    public static string FACEBOOK_URL = "https://github.com/Kartoro/COMP90018_MobileApp";
+    public static string TWITTER_URL = "https://github.com/Kartoro/COMP90018_MobileApp";
+    public static string CONTACT_URL = "https://github.com/Kartoro/COMP90018_MobileApp";
 }
