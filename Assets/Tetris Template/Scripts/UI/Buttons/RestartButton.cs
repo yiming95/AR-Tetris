05a79d47e68addc09835bda6397a9fd7dff9d27e
Owen <yuhuaqing411739@gmail.com>
2019-10-23 16:29:41 +1100

    bind local lead

--- Assets/Tetris Template/Scripts/UI/Buttons/RestartButton.cs
+++ Assets/Tetris Template/Scripts/UI/Buttons/RestartButton.cs
@@ -16,32 +16,4 @@ public class RestartButton : MonoBehaviour {
         Managers.Game.SetState(typeof(GamePlayState));
         Managers.UI.inGameUI.gameOverPopUp.SetActive(false);
     }
-<<<<<<< HEAD
-=======
-
-    public void OnClickARRestartButton()
-    {
-        //GameManager.difficulty = GameManager.difficultyTemp;
-        //Managers.Audio.PlayUIClick();
-        //GameObject.Find("Grid");
-        //Managers.Score.ResetCurrentScore();
-        //Destroy(SpawnManager.next);
-        //SpawnManager.isFirst = true;
-        //Managers.Game.isGameActive = false;
-        //Managers.Game.SetState(typeof(GamePlayState));
-        //Managers.UI.inGameUI.gameOverPopUp.SetActive(false);
-
-
-        //GameManager.difficulty = GameManager.difficultyTemp;
-        Managers.Audio.PlayUIClick();
-        Managers.Grid.ClearBoard();
-        Managers.Score.ResetCurrentScore();
-        Destroy(SpawnManager.next);
-        SpawnManager.isFirst = true;
-        Managers.Game.isGameActive = false;
-        Managers.Game.SetState(typeof(GamePlayState));
-        Managers.UI.inGameUI.gameOverPopUp.SetActive(false);
-    }
-
->>>>>>> 223776d31154ec5965166130610600a68d2721ce
 }
