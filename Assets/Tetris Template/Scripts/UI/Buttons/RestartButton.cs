3c98b0ae5ebf3ada8f4b25f8abc4d1cefd0916fa
Yiming <zym1995osl@163.com>
2019-10-23 15:46:04 +1100

    restart button

--- Assets/Tetris Template/Scripts/UI/Buttons/RestartButton.cs
+++ Assets/Tetris Template/Scripts/UI/Buttons/RestartButton.cs
@@ -16,4 +16,17 @@ public class RestartButton : MonoBehaviour {
         Managers.Game.SetState(typeof(GamePlayState));
         Managers.UI.inGameUI.gameOverPopUp.SetActive(false);
     }
+
+    public void AROnClickRestartButton()
+    {
+        //GameManager.difficulty = GameManager.difficultyTemp;
+        Managers.Audio.PlayUIClick();
+        Managers.Grid.ClearBoard();
+        Managers.Score.ResetCurrentScore();
+        Destroy(SpawnManager.next);
+        SpawnManager.isFirst = true;
+        Managers.Game.isGameActive = false;
+        Managers.Game.SetState(typeof(GamePlayState));
+        Managers.UI.inGameUI.gameOverPopUp.SetActive(false);
+    }
 }
