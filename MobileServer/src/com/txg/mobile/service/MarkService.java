372521683b312d24d1de84dae108f23c7bea0884
TXG-DYY <2430070746@qq.com>
2019-10-23 18:23:25 +1100

    Server with Local Result

--- MobileServer/src/com/txg/mobile/service/MarkService.java
+++ MobileServer/src/com/txg/mobile/service/MarkService.java
@@ -32,15 +32,12 @@ public class MarkService {
 
 	public Integer findRank(String mobile) {
 		SqlHelper sh = new SqlHelper();
-		String sql = "select * from(\n" + 
-				"select * ,@current_rank := @current_rank + 1 as rank\n" + 
-				"From mark m, (select @current_rank := 0)q\n" + 
-				"order by mark desc) AS r\n" + 
-				"where mobile = ?";
-		String[] params = {mobile};
+		String sql = "select * from(\n" + "select * ,@current_rank := @current_rank + 1 as rank\n"
+				+ "From mark m, (select @current_rank := 0)q\n" + "order by mark desc) AS r\n" + "where mobile = ?";
+		String[] params = { mobile };
 		ResultSet rs = sh.queryExecute(sql, params);
 		try {
-			if(rs.next()) {
+			if (rs.next()) {
 				return rs.getInt("rank");
 			}
 		} catch (Exception e) {
@@ -50,11 +47,11 @@ public class MarkService {
 		return null;
 	}
 
-	public boolean insertMark(String mobile, String mark) {
+	public boolean insertMark(String mobile, String mark, Double gps_x, Double gps_y) {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		SqlHelper sh = new SqlHelper();
-		String sql = "insert into mark values(?,?,?)";
-		String[] params = { mobile, mark, sdf.format(new Date()) };
+		String sql = "insert into mark values(?,?,?,?,?)";
+		Object[] params = { mobile, mark, sdf.format(new Date()), gps_x, gps_y };
 		if (sh.updateExecute(sql, params) > 0) {
 			sh.close();
 			return true;
@@ -64,11 +61,11 @@ public class MarkService {
 		}
 	}
 
-	public boolean updateMark(String mobile, String mark) {
+	public boolean updateMark(String mobile, String mark, Double gps_x, Double gps_y) {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		SqlHelper sh = new SqlHelper();
-		String sql = "update mark set mark = ?, lastUpdate = ? where mobile = ?";
-		String[] params = { mark, sdf.format(new Date()), mobile };
+		String sql = "update mark set mark = ?, lastUpdate = ?, gps_x = ?, gps_y = ? where mobile = ?";
+		Object[] params = { mark, sdf.format(new Date()), gps_x, gps_y, mobile };
 		if (sh.updateExecute(sql, params) > 0) {
 			sh.close();
 			return true;
@@ -101,5 +98,74 @@ public class MarkService {
 			sh.close();
 		}
 		return result;
+	}
+
+	public Integer findLocalRank(String mobile, Double gps_x, Double gps_y, Double range) {
+		// TODO Auto-generated method stub
+		SqlHelper sh = new SqlHelper();
+		String sql = "select * from "
+				+ "(select * ,@current_rank := @current_rank + 1 as rank "
+				+ "from mark m, (select @current_rank := 0)q "
+				+ "where m.gps_x between ?-? and ?+? and m.gps_y between ?-? and ?+?"
+				+ "order by mark desc) AS r where mobile = ?";
+		Object[] params = {gps_x, range, gps_x, range, gps_y, range, gps_y, range, mobile };
+		ResultSet rs = sh.queryExecute(sql, params);
+		try {
+			if (rs.next()) {
+				return rs.getInt("rank");
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public List<Player> getLocalTopMark(Integer top, Double gps_x, Double gps_y, Double range) {
+		// TODO Auto-generated method stub
+		List<Player> result = new ArrayList<Player>();
+
+		SqlHelper sh = new SqlHelper();
+		String sql = " select * from mark m left join player p on m.mobile = p.mobile "
+				+ "where m.gps_x between ?-? and ?+? and m.gps_y between ?-? and ?+? order by mark desc limit ?";
+		Object[] params = {gps_x, range, gps_x, range, gps_y, range, gps_y, range, top};
+		try {
+			ResultSet rs = sh.queryExecute(sql, params);
+			while (rs.next()) {
+				Player p = new Player();
+				p.setMobile(rs.getString("mobile"));
+				p.setUsername(rs.getString("username"));
+				p.setMark(rs.getInt("mark"));
+				result.add(p);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			sh.close();
+		}
+		return result;
+	}
+
+	public List<Double> getLocalCoor(String mobile) {
+		// TODO Auto-generated method stub
+		SqlHelper sh = new SqlHelper();
+		String sql = "select * from mark where mobile = ?";
+		ArrayList<Double> list = new ArrayList<Double>();
+		String[] params = { mobile };
+		ResultSet rs = sh.queryExecute(sql, params);
+		try {
+			if (rs.next()) {
+				list.add(rs.getDouble("gps_x"));
+				list.add(rs.getDouble("gps_y"));
+				return list;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			sh.close();
+		}
+		return null;
 	}
 }
--- MobileServer/src/com/txg/mobile/servlet/RequestLocalServlet.java
+++ MobileServer/src/com/txg/mobile/servlet/RequestLocalServlet.java
@@ -9,57 +9,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.txg.mobile.domain.Player;
 import com.txg.mobile.service.MarkService;
 import com.txg.mobile.utils.ChangeToJSON;
 
-
-public class UploadServlet extends HttpServlet {
+/**
+ * Servlet implementation class RequestLocalServlet
+ */
+public class RequestLocalServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	public MarkService ms = new MarkService();
+	private MarkService ms = new MarkService();
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doPost(request, response);
 	}
 
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("RequestLocalServlet");
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
-		System.out.println("Upload");
 		String data = request.getParameter("data");
-		System.out.println("up"+data);
 		JSONObject dataObject = JSONObject.parseObject(data);
 		String mobile = dataObject.getString("mobile");
-		String mark = dataObject.getString("mark");
-		Double gps_x = Double.parseDouble(dataObject.getString("gps_x"));
-		Double gps_y = Double.parseDouble(dataObject.getString("gps_y"));
-		PrintWriter writer = response.getWriter();
 		
-		if (ms.findPlayer(mobile)) {
-			if (ms.updateMark(mobile, mark, gps_x, gps_y)) {
-				//First global if friends then add.
-				//List<Player> list = ms.getGlobalTopMark(5);
-				//Integer rank = ms.findRank(mobile);
-				writer.write("Upload Success");
-			}else {
-				writer.write("Update Fail");
-			}
-		}else {
-			if(ms.insertMark(mobile, mark, gps_x, gps_y)) {
-				//List<Player> list = ms.getGlobalTopMark(5);
-				//Integer rank = ms.findRank(mobile);
-				writer.write("Upload Success");
-			}else {
-				writer.write("Insert Fail");
-			}
+		List<Double> coordinate = ms.getLocalCoor(mobile);
+		PrintWriter writer = response.getWriter();
+		if(coordinate.get(0) == 0 && coordinate.get(1) ==0) {
+			writer.write("None@[]");
+			return;
 		}
 		
-		
+		Integer rank = ms.findLocalRank(mobile, coordinate.get(0),coordinate.get(1), 0.3);
+		List<Player> list = ms.getLocalTopMark(5, coordinate.get(0),coordinate.get(1), 0.3);
+		writer.write(rank+"@"+ChangeToJSON.playerToJSON(list).toString());
 	}
 
 }
