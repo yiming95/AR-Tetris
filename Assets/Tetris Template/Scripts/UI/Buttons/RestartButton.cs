223776d31154ec5965166130610600a68d2721ce
Yiming <zym1995osl@163.com>
2019-10-23 16:19:55 +1100

    change restart button
    AR MODE: restart button, works at the menu bar

--- Assets/Tetris Template/Scripts/UI/Buttons/RestartButton.cs
+++ Assets/Tetris Template/Scripts/UI/Buttons/RestartButton.cs
@@ -17,19 +17,22 @@ public class RestartButton : MonoBehaviour {
         Managers.UI.inGameUI.gameOverPopUp.SetActive(false);
     }
 
-<<<<<<< HEAD
     public void OnClickARRestartButton()
     {
-        GameManager.difficulty = GameManager.difficultyTemp;
-        Managers.Audio.PlayUIClick();
-        GameObject.Find("Grid");
-=======
-    public void AROnClickRestartButton()
-    {
+        //GameManager.difficulty = GameManager.difficultyTemp;
+        //Managers.Audio.PlayUIClick();
+        //GameObject.Find("Grid");
+        //Managers.Score.ResetCurrentScore();
+        //Destroy(SpawnManager.next);
+        //SpawnManager.isFirst = true;
+        //Managers.Game.isGameActive = false;
+        //Managers.Game.SetState(typeof(GamePlayState));
+        //Managers.UI.inGameUI.gameOverPopUp.SetActive(false);
+
+
         //GameManager.difficulty = GameManager.difficultyTemp;
         Managers.Audio.PlayUIClick();
         Managers.Grid.ClearBoard();
->>>>>>> e9b5278c3f8e44598af73fca4aec9a6938381080
         Managers.Score.ResetCurrentScore();
         Destroy(SpawnManager.next);
         SpawnManager.isFirst = true;
@@ -37,8 +40,5 @@ public class RestartButton : MonoBehaviour {
         Managers.Game.SetState(typeof(GamePlayState));
         Managers.UI.inGameUI.gameOverPopUp.SetActive(false);
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> e9b5278c3f8e44598af73fca4aec9a6938381080
 }
